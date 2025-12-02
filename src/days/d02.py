4b4ee8debad8453d7baca41162a1e5c7a42be95b
ErikGimbert <erik.gimbert.github@ergt.eu>
2025-12-02 21:23:36 +0100

    day 2, part 2

--- src/days/d02.py
+++ src/days/d02.py
@@ -30,10 +30,37 @@ def sum_invalid_ids(ids_range: Iterator[tuple[int, int]]) -> int:
     return sum(itertools.chain.from_iterable(map(find_invalid_ids, ids_range)))
 
 
+# ==============================================================
+# Part Two
+# ==============================================================
+
+RE_INVALID_ID_V2 = re.compile(r"^(\d+)\1+$")
+
+
+def find_invalid_ids_v2(ids_range: tuple[int, int]) -> Iterator[int]:
+    for id in range(ids_range[0], ids_range[1] + 1):
+        if RE_INVALID_ID_V2.match(str(id)):
+            yield id
+
+
+def sum_invalid_ids_v2(ids_range: Iterator[tuple[int, int]]) -> int:
+    return sum(itertools.chain.from_iterable(map(find_invalid_ids_v2, ids_range)))
+
+
+# ==============================================================
+# Main function
+# ==============================================================
+
+
 def run():
     ids_range = load_data("./inputs/day_02.txt")
     result = sum_invalid_ids(ids_range)
-    pr_day(2, "Sum of invalid IDs:", result)
+    pr_day(2, "Part One: Sum of invalid IDs:", result)
+
+    # Part Two
+    ids_range = load_data("./inputs/day_02.txt")
+    result = sum_invalid_ids_v2(ids_range)
+    pr_day(2, "Part Two: Sum of invalid IDs:", result)
 
 
 if __name__ == "__main__":
--- tests/test_day_02.py
+++ tests/test_day_02.py
@@ -1,6 +1,12 @@
 import pytest
 
-from days.d02 import find_invalid_ids, load_data, sum_invalid_ids
+from days.d02 import (
+    find_invalid_ids,
+    find_invalid_ids_v2,
+    load_data,
+    sum_invalid_ids,
+    sum_invalid_ids_v2,
+)
 from tests.utils import with_tmp_file
 
 
@@ -81,3 +87,46 @@ class TestSumInvalidIds:
         result = sum_invalid_ids(iter(ids_ranges))
         assert result == expected_sum
         assert result == 1227775554
+
+
+# ==============================================================
+# Part Two
+# ==============================================================
+
+
+RANGES_WITH_INVALID_IDS_V2 = [
+    ((11, 22), [11, 22]),
+    ((95, 115), [99, 111]),
+    ((998, 1012), [999, 1010]),
+    ((1188511880, 1188511890), [1188511885]),
+    ((222220, 222224), [222222]),
+    ((1698522, 1698528), []),
+    ((446443, 446449), [446446]),
+    ((38593856, 38593862), [38593859]),
+    ((565653, 565659), [565656]),
+    ((824824821, 824824827), [824824824]),
+    ((2121212118, 2121212124), [2121212121]),
+]
+
+
+class TestFindInvalidIdsV2:
+    @pytest.mark.parametrize(
+        "ids_range,expected",
+        RANGES_WITH_INVALID_IDS_V2,
+    )
+    def test_find_invalid_ids_v2(self, ids_range, expected):
+        result = list(find_invalid_ids_v2(ids_range))
+        assert result == expected
+
+
+class TestSumInvalidIdsV2:
+    def test_sum_invalid_ids_v2(self):
+        ids_ranges = []
+        expected_sum = 0
+        for ids_range, invalid_ids in RANGES_WITH_INVALID_IDS_V2:
+            ids_ranges.append(ids_range)
+            expected_sum += sum(invalid_ids)
+
+        result = sum_invalid_ids_v2(iter(ids_ranges))
+        assert result == expected_sum
+        assert result == 4174379265
