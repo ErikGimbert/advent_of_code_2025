dac5e19c4ebc983d885c44b9e5a15b7e504dc160
ErikGimbert <erik.gimbert.github@ergt.eu>
2025-12-04 15:06:13 +0100

    test/_template.py: improvement

--- tests/_template.py
+++ tests/_template.py
@@ -1,30 +1,54 @@
-import pytest  # noqa: F401
+import pytest
 
 from days._template import load_data
 from tests.utils import with_tmp_file
 
 # ===========================================================================
-# MARK: Part One
+# MARK: Load Data
 # ===========================================================================
+
+EXAMPLE = """"""  # TODO add example input data
+EXAMPLE_SPLIT = [
+    # TODO add example input data
+]
 
 
 class TestLoadData:
     def test_load_from_tmp(self):
         def f(file_path: str) -> None:
             result = list(load_data(file_path))
-            assert result == []
+            assert result == EXAMPLE_SPLIT
 
-        with_tmp_file("""""")(f)()
+        with_tmp_file(EXAMPLE)(f)()
 
     def test_load_input(self):
         result = list(load_data())
-        assert len(result) > 1000
+        assert len(result) > 10
 
 
-# TODO add tests for part one
+# ===========================================================================
+# MARK: Part One
+# ===========================================================================
+
+
+class TestPartOne:
+    EXAMPLE_EXPECTED = {
+        # TODO add example input data
+    }
+
+    @pytest.mark.parametrize(
+        "input",
+        EXAMPLE,
+    )
+    def test_example(self, input):
+        result = 0  # TODO replace with processing call
+        assert result == self.EXAMPLE_EXPECTED[input]
+
 
 # ===========================================================================
 # MARK: Part Two
 # ===========================================================================
 
-# TODO add tests for part two
+
+class TestPartTwo:
+    pass
