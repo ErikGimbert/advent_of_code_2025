57682f077b7f692074765b9f7f1048d3f923dc22
ErikGimbert <erik.gimbert.github@ergt.eu>
2025-12-11 10:06:42 +0100

    Day 6, init

---
+++ src/days/d06.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+from typing import Iterator
+
+import utils.print as pr
+from utils import file
+
+DAY: int = 6
+DAY_PADDED = f"{DAY:02d}"
+
+
+def load_data(file_path: str | Path = file.input_path(DAY)) -> Iterator:
+    input = file.to_list(file_path)
+    # TODO implement data loading
+    return input
+
+
+# ==============================================================
+# MARK: Part One
+# ==============================================================
+
+
+def part_one(input: Iterator) -> int:
+    raise NotImplementedError  # TODO implement part one
+    return 0
+
+
+# ==============================================================
+# MARK: Part Two
+# ==============================================================
+
+
+def part_two(input: Iterator) -> int:
+    raise NotImplementedError  # TODO implement part two
+    return 0
+
+
+# ==============================================================
+# MARK: Main function
+# ==============================================================
+
+
+def run():
+    # Part One
+    input = load_data()
+    result = part_one(input)
+    pr.day(DAY, "Part One: ", result)
+
+    # Part Two
+    try:
+        input = load_data()
+        result = part_two(input)
+        pr.day(DAY, "Part Two: ", result)
+    except NotImplementedError:
+        pr.info("Part Two not implemented yet.")
